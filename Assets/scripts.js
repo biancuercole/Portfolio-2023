d2770d748d4b815fc7f578bbe47c688eabc0e5e3
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-03-31 23:05:29 -0300

    Sobre mi avanzado, iconos agregados

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,10 +1,13 @@
-const navbar = document.getElementById('navbar')
-const navbarheight = navbar.clientHeight;
 
-window.addEventListener('scroll', function() {
-    if (this.window.scrollY >= navbarheight) {
-        navbar.classList.add('navbar-expanded');
-    } else {
-        navbar.classList.remove('navbar-expanded');
-    }
+var boton1 = document.getElementById("boton1");
+var popup1 = document.getElementById("popup1");
+var close1 = document.getElementById("close1");
+
+boton1.addEventListener("click", function() {
+    popup1.style.display = "block";
+})
+
+close1.addEventListener("click", function() {
+    popup1.style.display = "none";
+
 });

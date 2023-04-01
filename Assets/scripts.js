44b9eaa314ed89c4672f200591f305722465f2e7
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-03-31 23:52:07 -0300

    JAVASCRIPT EMERGENTES COMENZADO

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,13 +1,56 @@
 
-var boton1 = document.getElementById("boton1");
-var popup1 = document.getElementById("popup1");
-var close1 = document.getElementById("close1");
+document.addEventListener("DOMContentLoaded", function() {
+    var boton1 = document.getElementById("boton1");
+    var popup1 = document.getElementById("popup1");
+    var close1 = document.getElementById("close1");
 
-boton1.addEventListener("click", function() {
+    boton1.addEventListener("click", function() {
     popup1.style.display = "block";
-})
+    });
 
-close1.addEventListener("click", function() {
+    close1.addEventListener("click", function() {
     popup1.style.display = "none";
-
+    });
 });
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var boton2 = document.getElementById("boton2");
+        var popup2 = document.getElementById("popup2");
+        var close2 = document.getElementById("close2");
+    
+        boton2.addEventListener("click", function() {
+        popup2.style.display = "block";
+        });
+    
+        close2.addEventListener("click", function() {
+        popup2.style.display = "none";
+        });
+    });
+ 
+    document.addEventListener("DOMContentLoaded", function() {
+        var boton3 = document.getElementById("boton3");
+        var popup3 = document.getElementById("popup3");
+        var close3 = document.getElementById("close3");
+    
+        boton3.addEventListener("click", function() {
+        popup3.style.display = "block";
+        });
+    
+        close3.addEventListener("click", function() {
+        popup3.style.display = "none";
+        });
+    }); 
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var boton4 = document.getElementById("boton4");
+        var popup4 = document.getElementById("popup4");
+        var close4 = document.getElementById("close4");
+    
+        boton4.addEventListener("click", function() {
+        popup4.style.display = "block";
+        });
+    
+        close4.addEventListener("click", function() {
+        popup4.style.display = "none";
+        });
+    });

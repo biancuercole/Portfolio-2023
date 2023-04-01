a7c3484053047ff85fe0a7d316aa97dfd60d5b6b
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-04-01 00:37:21 -0300

    JS DE EMERGENTES ARREGLADO

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,56 +1,56 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     var boton1 = document.getElementById("boton1");
-    var popup1 = document.getElementById("popup1");
-    var close1 = document.getElementById("close1");
+    var popup = document.getElementById("popup");
+    var close = document.getElementById("close");
 
     boton1.addEventListener("click", function() {
-    popup1.style.display = "block";
+    popup.style.display = "block";
     });
 
-    close1.addEventListener("click", function() {
-    popup1.style.display = "none";
+    close.addEventListener("click", function() {
+    popup.style.display = "none";
     });
 });
 
     document.addEventListener("DOMContentLoaded", function() {
         var boton2 = document.getElementById("boton2");
-        var popup2 = document.getElementById("popup2");
-        var close2 = document.getElementById("close2");
+        var popup = document.getElementById("popup");
+        var close = document.getElementById("close");
     
         boton2.addEventListener("click", function() {
-        popup2.style.display = "block";
+        popup.style.display = "block";
         });
     
-        close2.addEventListener("click", function() {
-        popup2.style.display = "none";
+        close.addEventListener("click", function() {
+        popup.style.display = "none";
         });
     });
  
     document.addEventListener("DOMContentLoaded", function() {
         var boton3 = document.getElementById("boton3");
-        var popup3 = document.getElementById("popup3");
-        var close3 = document.getElementById("close3");
+        var popup = document.getElementById("popup");
+        var close = document.getElementById("close");
     
         boton3.addEventListener("click", function() {
-        popup3.style.display = "block";
+        popup.style.display = "block";
         });
     
-        close3.addEventListener("click", function() {
-        popup3.style.display = "none";
+        close.addEventListener("click", function() {
+        popup.style.display = "none";
         });
     }); 
 
     document.addEventListener("DOMContentLoaded", function() {
         var boton4 = document.getElementById("boton4");
-        var popup4 = document.getElementById("popup4");
-        var close4 = document.getElementById("close4");
+        var popup = document.getElementById("popup");
+        var close = document.getElementById("close");
     
         boton4.addEventListener("click", function() {
-        popup4.style.display = "block";
+        popup.style.display = "block";
         });
     
-        close4.addEventListener("click", function() {
-        popup4.style.display = "none";
+        close.addEventListener("click", function() {
+        popup.style.display = "none";
         });
     });
